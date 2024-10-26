ede3a10a17f129cc3590d4b4353cd9f7f181c4d9
ilyas <email@example.com>
2024-10-26 19:28:30 +0300

    first commit app

---
+++ chatgpt/chat/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class Chat(models.Model):
+    message = models.CharField(max_length=100000)
+    response = models.CharField(max_length=100000)
+    create_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.message
+    
